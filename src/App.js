a2fc2f3e6419a7fd580ac7dcfd6fe788780c5ef7
john.lynas <johnlynas666@gmail.com>
2020-10-13 17:25:52 +0100

    Added fetch and user ids

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from './CardList';
-import robots from './robots';
 import SearchBox from './SearchBox'
 import './App.css'
 
@@ -10,9 +9,16 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        //  Make requests using fetch method
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     // Any time you are using React components  and you are defining you own functions, you  need to declare 
@@ -26,13 +32,17 @@ class App extends React.Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
 
-        return (
-            <div className='tc'>
-                <h1 className="f2">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return (<h1>Loading...</h1>)
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className="f2">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            )
+        }
 
     }
 }
